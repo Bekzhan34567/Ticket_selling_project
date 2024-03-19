50c3236ceb76d2ac3ca74a4292358e8b99015f34
Bekzhan34567 <113165090+Bekzhan34567@users.noreply.github.com>
2024-03-19 12:11:52 +0600

    Add files via upload

---
+++ Ticket-selling-app/src/main/java/kg/alatoo/ticketsellingapp/TicketSellingAppApplication.java
@@ -0,0 +1,13 @@
+package kg.alatoo.ticketsellingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSellingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketSellingAppApplication.class, args);
+    }
+
+}
---
+++ Ticket-selling-app/src/test/java/kg/alatoo/ticketsellingapp/TicketSellingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.alatoo.ticketsellingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSellingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
